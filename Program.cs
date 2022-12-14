e99aeec90bc20e2f4a505d487a50be7681bd9c5c
karpukhinda <karpuhinda@yandex.ru>
2022-12-14 15:50:23 +0300

    сохр

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все
-// натуральные числа в промежутке от N до 1.
+﻿// // Задача 64: Задайте значение N. Напишите программу, которая выведет все
+// // натуральные числа в промежутке от N до 1.
 
 
 // Console.Write("Введите число: ");
@@ -23,9 +23,34 @@
 
 // // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
+// Console.Write("Введите число M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
 
-//  Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// Console.Write("Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
 
+// SumFromMToN(m, n);
+
+// void SumFromMToN(int m, int n)
+// {
+//     Console.Write(SumMN(m - 1, n));
+// }
+
+// int SumMN(int m, int n)
+// {
+//     int res = m;
+//     if (m == n)
+//         return 0;
+//     else
+//     {
+//         m++;
+//         res = m + SumMN(m, n);
+//         return res;
+//     }
+// }
+
+
+//  Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
 Console.Write("Введите число M: ");
 int m = Convert.ToInt32(Console.ReadLine());
@@ -33,22 +58,24 @@ int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число N: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-SumFromMToN(m, n);
+AkkermanFunction(m,n);
 
-void SumFromMToN(int m, int n)
+void AkkermanFunction(int m, int n)
 {
-    Console.Write(SumMN(m - 1, n));
+    Console.Write(Akkerman(m, n)); 
 }
-
-int SumMN(int m, int n)
+int Akkerman(int m, int n)
 {
-    int res = m;
-    if (m == n)
-        return 0;
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
     else
     {
-        m++;
-        res = m + SumMN(m, n);
-        return res;
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
     }
 }
